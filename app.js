0651cdde1e8ee051e415e26415ee8acc2a2842fc
Kristiyan <bkristiqn7@gmail.com>
2022-10-15 14:03:20 +0300

    Fixed bugs and cleaned code

--- app.js
+++ app.js
@@ -12,93 +12,66 @@ const library = [];
 const addButtonElement = document.querySelector('#add-button');
 const booksListElement = document.querySelector('#books');
 const bookCardElement = document.querySelector('.add-book-card');
-const [readButtonElement, addBookButtonElement] = document.querySelectorAll('button[type=button]');
+const readButtonElement = document.querySelector('button[type=button]');
+const submitButtonElement = document.querySelector('#add');
 
-//Event listeners---------------------------------------------------------------------------
+//Event listeners--------------------------------------------------------------------------------------------
 addButtonElement.addEventListener('click', () => bookCardElement.style.display = 'block');
-readButtonElement.addEventListener('click', () => {
-    if (readButtonElement.textContent == 'Not read') {
-        readButtonElement.classList.add('green');
-        readButtonElement.classList.remove('red');
-        readButtonElement.textContent = 'Read';
-        // debugger;
-    } else {
-        readButtonElement.classList.add('red');
-        readButtonElement.classList.remove('green');
-        readButtonElement.textContent = 'Not read';
-        // debugger;
-    }
-});
-addBookButtonElement.addEventListener('click', () => {
+readButtonElement.addEventListener('click', changeColor);
+submitButtonElement.addEventListener('click', addBookToLibrary);
+
+//Functions--------------------------------------------------------------------------------------------------
+function changeColor(e) {
+    e.target.textContent = e.target.textContent == 'Not read' ? 'Read' : 'Not read';
+    e.target.classList.toggle('green');
+}
+function addBookToLibrary(e) {
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const pages = Number(document.querySelector('#pages').value);
-    const isRead = readButtonElement.classList.contains('green') ? true : false;
+    const isRead = e.target.parentElement.children[3].classList.contains('green') ? true : false;
     if (!title || !author || !pages) return;
 
-    const book = new Book(title, author, pages, isRead)
-    addBookToLibrary(book);
-
-
-    bookCardElement.style.display = 'none';
-    readButtonElement.classList.add('red');
-    readButtonElement.classList.remove('green');
-    readButtonElement.textContent = 'Not read';
+    const book = new Book(title, author, pages, isRead);
+    library.push(book);
+    displayBook(book);
     document.querySelectorAll('input').forEach(el => el.value = '');
-});
-
-//Functions---------------------------------------------------------------------------------
-function addBookToLibrary(bookObj) {
-    library.push(bookObj);
-    displayBook(bookObj);
+    if (readButtonElement.classList.contains('green')) readButtonElement.classList.toggle('green');
+    readButtonElement.textContent = 'Not read';
+    e.target.parentElement.parentElement.style.display = 'none';
 }
 function displayBook(bookObj) {
-    // Container
+    // Create card
     const div = document.createElement('div');
     div.classList.add('card');
-    //Title
-    const title = document.createElement('p');
+    const title = document.createElement('p');          //Title
     title.textContent = `Title: ${bookObj.title}`;
-
-    //Author
-    const author = document.createElement('p');
+    const author = document.createElement('p');         //Author
     author.textContent = `Author: ${bookObj.author}`;
-
-    //Pages
-    const pages = document.createElement('p');
+    const pages = document.createElement('p');          //Pages
     pages.textContent = `Pages: ${bookObj.pages}`;
-
-    //isRead
-    const isRead = document.createElement('button');
+    const isRead = document.createElement('button');    //Is read
     if (bookObj.isRead) {
         isRead.textContent = 'Read';
         isRead.classList.add('green');
     } else {
         isRead.textContent = 'Not read';
-        isRead.classList.add('red');
+        isRead.style.backgroundColor = 'var(--red)';
     }
-    isRead.addEventListener('click', () => {
-        if (isRead.textContent == 'Not read') {
-            isRead.classList.add('green');
-            isRead.classList.remove('red');
-            isRead.textContent = 'Read';
-            // debugger;
-        } else {
-            isRead.classList.add('red');
-            isRead.classList.remove('green');
-            isRead.textContent = 'Not read';
-            // debugger;
-        }
-    });
-    //Remove
-    const removeButton = document.createElement('button');
+    const removeButton = document.createElement('button');//Remove
     removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', (e) => {
-        e.target.parentElement.remove();
-    });
-    //Append
+    removeButton.addEventListener('click', removeElement);
+
+    //Append card
     div.append(title, author, pages, isRead, removeButton);
     booksListElement.append(div);
-
-    document.querySelector('#books').append(div);
+}
+function removeElement(e) {
+    e.target.parentElement.remove();
+    const elementTitle = e.target.parentElement.children[0].textContent.split(': ')[1];
+    const indexOfObject = library.findIndex(object => {
+        return object.title == elementTitle;
+    });
+    library.splice(indexOfObject, 1);
+    console.log(library);
 }
